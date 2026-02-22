6e3fb16f18deba5865ee00d187e64bdf185204b7
Lilya Baranovskaya <lili.baranovskaja@gmail.com>
2026-02-22 20:01:09 +0300

    diplom code

---
+++ orders/wsgi.py
@@ -0,0 +1,8 @@
+"""
+WSGI конфигурация для деплоя на production серверах.
+"""
+import os
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'orders.settings')
+application = get_wsgi_application()
