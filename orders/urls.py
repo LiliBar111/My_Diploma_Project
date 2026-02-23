c83b3c0da056f74c3a46c34df6e7697661f4e537
Lilya Baranovskaya <lili.baranovskaja@gmail.com>
2026-02-23 22:45:31 +0300

    new diplom code

--- orders/urls.py
+++ orders/urls.py
@@ -20,3 +20,19 @@ schema_view = get_schema_view(
     public=True,
     permission_classes=[permissions.AllowAny],
 )
+
+urlpatterns = [
+    # Админка Django
+    path('admin/', admin.site.urls),
+
+    # API моего приложения
+    path('api/v1/', include('backend.urls')),
+
+    # Документация API в двух форматах
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
