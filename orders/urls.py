ffd79d802ce760a042b580239ba75d562ec43120
Lilya Baranovskaya <lili.baranovskaja@gmail.com>
2026-02-27 20:46:57 +0300

    add social authentication with Google and GitHub

--- orders/urls.py
+++ orders/urls.py
@@ -31,8 +31,18 @@ urlpatterns = [
     # Документация API в двух форматах
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # Социальная аутентификация (Google, GitHub)
+    path('auth/', include('social_django.urls', namespace='social')),
 ]
 
+# Добавляем маршруты для silk (профилирование) только в режиме отладки
+if settings.DEBUG:
+    urlpatterns += [
+        path('silk/', include('silk.urls', namespace='silk')),
+    ]
+
+# Обслуживание медиа и статики в режиме разработки
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
