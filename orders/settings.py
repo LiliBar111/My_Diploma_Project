f53f7d252a041f8dd622018c0c0a794b8db92334
Lilya Baranovskaya <lili.baranovskaja@gmail.com>
2026-02-28 16:55:44 +0300

    add throttling, social auth, baton, cacheops, sentry, silk

--- orders/settings.py
+++ orders/settings.py
@@ -1,23 +1,15 @@
-"""
-Настройки Django проекта.
-Здесь я определяю все конфигурации для работы приложения.
-"""
 import os
 from pathlib import Path
 from dotenv import load_dotenv
 
-# Загружаю переменные из .env файла
 load_dotenv()
 
-# Базовая директория проекта
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Безопасность
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-default-key')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
-# Установленные приложения
 INSTALLED_APPS = [
     # Стандартные приложения Django
     'django.contrib.admin',
@@ -26,36 +18,60 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',  # требуется для social-auth
 
     # Сторонние приложения
-    'rest_framework',  # Django REST Framework
-    'django_filters',  # Фильтрация для API
-    'corsheaders',  # CORS заголовки
-    'drf_yasg',  # Документация Swagger
-    'django_rest_passwordreset',  # Сброс пароля
+    'rest_framework',
+    'django_filters',
+    'corsheaders',
+    'drf_yasg',
+    'django_rest_passwordreset',
 
-    # Мое приложение
+    # Новые приложения
+    'baton',                        # django-baton для админки
+    'baton.autodiscover',           # автоподключение
+    'social_django',                 # социальная аутентификация
+    'easy_thumbnails',               # миниатюры
+    'cacheops',                      # кэширование запросов
+    'silk',                          # профилирование
     'backend',
 ]
 
-# Middleware компоненты
+# Настройки для django-baton (улучшенная админка)
+BATON = {
+    'SITE_HEADER': 'Администрирование закупок',
+    'SITE_TITLE': 'Закупки',
+    'INDEX_TITLE': 'Панель управления',
+    'SUPPORT_HREF': 'https://github.com/ваш-username/orders',
+    'COPYRIGHT': '© 2024 Ваше Имя',
+    'POWERED_BY': '<a href="https://github.com/ваш-username">Ваш проект</a>',
+    'CONFIRM_UNSAVED': True,
+    'SHOW_MULTIPART_UPLOADING': True,
+    'ENABLE_IMAGES_PREVIEW': True,
+    'CHANGELIST_FILTERS_IN_MODAL': True,
+    'MENU_ALWAYS_COLLAPSED': False,
+    'MENU_TITLE': 'Меню',
+    'MESSAGES_TOASTS': False,
+    'GRAVATAR_DEFAULT_IMG': 'identicon',
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'corsheaders.middleware.CorsMiddleware',  # Обработка CORS
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # Middleware для silk
+    'silk.middleware.SilkyMiddleware',
 ]
 
-# Разрешаю все CORS запросы в режиме разработки
 CORS_ALLOW_ALL_ORIGINS = DEBUG
-
 ROOT_URLCONF = 'orders.urls'
 
-# Настройки шаблонов
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -67,6 +83,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                # Для социальной аутентификации
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -74,7 +93,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'orders.wsgi.application'
 
-# Настройки базы данных PostgreSQL
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -86,7 +104,6 @@ DATABASES = {
     }
 }
 
-# Валидация паролей
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -94,20 +111,18 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-# Локализация
 LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
 USE_TZ = True
 
-# Статические и медиа файлы
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'backend.User'  # Использую кастомную модель пользователя
+AUTH_USER_MODEL = 'backend.User'
 
 # Настройки REST Framework
 REST_FRAMEWORK = {
@@ -119,7 +134,17 @@ REST_FRAMEWORK = {
         'rest_framework.filters.SearchFilter',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,  # По 20 товаров на странице
+    'PAGE_SIZE': 20,
+
+    # Добавляем тротлинг
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',   # Неавторизованные: 100 запросов в день
+        'user': '1000/day',  # Авторизованные: 1000 запросов в день
+    }
 }
 
 # Настройки Celery
@@ -134,3 +159,56 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'noreply@orders.ru')
+
+# Социальная аутентификация
+AUTHENTICATION_BACKENDS = (
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.github.GithubOAuth2',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('GOOGLE_OAUTH2_KEY', '')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('GOOGLE_OAUTH2_SECRET', '')
+SOCIAL_AUTH_GITHUB_KEY = os.getenv('GITHUB_KEY', '')
+SOCIAL_AUTH_GITHUB_SECRET = os.getenv('GITHUB_SECRET', '')
+
+# Перенаправление после логина
+LOGIN_REDIRECT_URL = '/api/v1/user/profile/'
+LOGOUT_REDIRECT_URL = '/'
+
+# Настройки django-cacheops
+CACHEOPS_REDIS = os.getenv('REDIS_URL', 'redis://redis:6379/0')
+CACHEOPS_DEFAULTS = {
+    'timeout': 60*60  # 1 час
+}
+CACHEOPS = {
+    'backend.productinfo': {'ops': 'get', 'timeout': 60*15},  # кэшировать get-запросы к ProductInfo на 15 минут
+    'backend.category': {'ops': 'all', 'timeout': 60*60},
+    'backend.shop': {'ops': 'all', 'timeout': 60*60},
+}
+CACHEOPS_ENABLED = True
+
+# Sentry
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+from sentry_sdk.integrations.celery import CeleryIntegration
+
+SENTRY_DSN = os.getenv('SENTRY_DSN', '')
+if SENTRY_DSN:
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration(), CeleryIntegration()],
+        traces_sample_rate=1.0,
+        send_default_pii=True,
+    )
+
+# Настройки для easy-thumbnails
+THUMBNAIL_ALIASES = {
+    '': {
+        'avatar': {'size': (100, 100), 'crop': True},
+        'product_thumbnail': {'size': (200, 200), 'crop': True},
+    },
+}
+
+# ID сайта для django.contrib.sites
+SITE_ID = 1
